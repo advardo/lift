0265096cc8a9ced8920005e6bee68238e1effceb
advardo <advardo@ukr.net>
2017-01-29 20:51:34 +0200

    added buttons and logic of speed

--- lift/Form1.Designer.cs
+++ lift/Form1.Designer.cs
@@ -33,15 +33,27 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(14, 20);
+            this.pictureBox2.Size = new System.Drawing.Size(16, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -56,11 +68,151 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(45, 392);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(45, 372);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(45, 352);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(45, 332);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(45, 312);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(45, 292);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(45, 272);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 9;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(45, 252);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(31, 17);
+            this.radioButton8.TabIndex = 10;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(45, 232);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 11;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(45, 212);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(37, 17);
+            this.radioButton10.TabIndex = 12;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "10";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(319, 442);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
@@ -68,6 +220,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +228,17 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- lift/Form1.cs
+++ lift/Form1.cs
@@ -12,27 +12,29 @@ namespace lift
 {
     public partial class Form1 : Form
     {
-        int speed = 0;
+        int speed = 1;
         int floorsize = 20;
         Random rf = new Random();
-        
-            
-  
+        int xofperson = 25;
+        int yofperson = 12;
+        int nfloor = 0;
+        int liftnow;
+
+
         public Form1()
         {
             InitializeComponent();
             int randfloor = rf.Next(0, 19);
-            pictureBox2.Location = new Point(14, randfloor * floorsize + 12);
+            liftnow = randfloor * floorsize + 12;
+            pictureBox2.Location = new Point(14, liftnow);
 
-          
+
+
         }
-        
+
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            Pen fp = new Pen(Color.IndianRed, 1);
-            System.Drawing.Graphics person = this.CreateGraphics();
-            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
-            person.DrawImage(per, 50, 50);
+
             Pen p = new Pen(Color.Black, 2);
             System.Drawing.Graphics line = this.CreateGraphics();
             for (int i = 0; i <= 20; i++)
@@ -43,24 +45,180 @@ namespace lift
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
 
-            speed = 1;
-            timer1.Interval = 10;
-            timer1.Tick += timer1_Tick;
-            timer1.Start();
+
+
 
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (pictureBox2.Location.Y <= 412-20)
-                pictureBox2.Top += speed;
-            else timer1.Stop();
+
+            // Console.WriteLine("aa : ", Convert.ToString(timer1.Tick));
+            timer1.Interval = 10;
+
+            if (pictureBox2.Location.Y != 412 - floorsize * nfloor)
+            {
+                if (liftnow < 412 - floorsize * nfloor)
+                    pictureBox2.Top += speed;
+                else pictureBox2.Top -= speed;
+            }
+            else
+            {
+                liftnow = 412 - floorsize * nfloor;
+                // label1.Text = timer1_Tick.;
+                //timer1.Tick -= timer1_Tick;
+                timer1.Stop();
+
+
+
+
+
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+        }
+
+
+        //______________________--------------------------_______________________________________________
+        //
+        //                        RADIO ----BUTTONS
+        //______________________--------------------------_______________________________________________
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 1;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton1.Visible = false;
+
+
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 2;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton2.Visible = false;
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 3;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton3.Visible = false;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 4;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton4.Visible = false;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 5;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton5.Visible = false;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 6;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton6.Visible = false;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 7;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton7.Visible = false;
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            nfloor = 8;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton8.Visible = false;
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 9;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton9.Visible = false;
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Tick -= timer1_Tick;
+            nfloor = 10;
+            System.Drawing.Graphics person = this.CreateGraphics();
+            Image per = Image.FromFile(@"C:\Users\advar\Desktop\per.png");
+            person.DrawImage(per, xofperson, yofperson + floorsize * (20 - nfloor), 20, 20);
+
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+            radioButton10.Visible = false;
         }
     }
 }
